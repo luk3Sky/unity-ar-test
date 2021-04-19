0352f0c0f0feb7a3119b009423320ffbcfeb917f
luk3Sky <ganindudananja@gmail.com>
2021-04-20 02:31:23 +0530

    CreateAsset script for AssetBundle testing

---
+++ Unity Vuforia AR Project/Assets/Scripts/ClearAsset.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearAsset : MonoBehaviour
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("This is C#");
+    }
+}
